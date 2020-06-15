c0df099bc6db91a2ebfca454752fbb409d7defe3
KingJS226 <meimei95226@gmail.com>
2020-06-15 15:07:25 +0800

    refactor: simple react-typescript project

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import './App.css';
+import { Switch, Route, withRouter, RouteComponentProps, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Create from './components/customer/Create';
+import EditCustomer from './components/customer/Edit';
+
+
+class App extends React.Component<RouteComponentProps<any>> {
+  public render() {
+    return (
+      <div>
+        <nav>
+          <ul className="container">
+            <li>
+              <Link to={'/'}> Customer List </Link>
+            </li>
+            <li>
+              <Link to={'/create'}> Create Customer </Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/create'} exact component={Create} />
+          <Route path={'/edit/:id'} exact component={EditCustomer} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
